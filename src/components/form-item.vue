6960344d0c7e165f71988ac55d4e91e5378d9e89
liuzhao <liuzhao@lnkun.com>
2018-12-29 16:32:45 +0800

    给formitem组件添加缓存事件。

--- src/components/form-item.vue
+++ src/components/form-item.vue
@@ -9,16 +9,31 @@
   </div>
 </template>
 <script>
+  import Emitter from "../mixins/emitter";
+
   export default {
     name: "VFormItem",
+    mixins: [ Emitter ],
     props: {
       label: {
         type: String,
-        default: ''
+        default: ""
       },
       prop: {
         type: String
       }
+    },
+    mounted() {
+      // 组件渲染时，将实例缓存在Form中
+      if(this.prop) {
+        // 如果没有传入prop，则无需校验，也就无需缓存。
+        this.dispatch("vForm", "on-form-item-add", this);
+      }
+    },
+    // 组件销毁前，将实例从form的缓存中移除
+    beforeDestroy() {
+      this.dispatch("vForm", "on-form-item-remove", this);
     }
+
   }
 </script>
