40393ca187125101d019c566a6dd9d8b226d7bf8
liuzhao <liuzhao@lnkun.com>
2019-01-17 16:26:45 +0800

    添加checkbox

---
+++ src/components/checkbox.vue
@@ -0,0 +1,71 @@
+<template>
+  <label>
+    <span>
+      <input
+          type="checkbox"
+          :disabled="disabled"
+          :checked="currentValue"
+          @change="change">
+    </span>
+    <slot></slot>
+  </label>
+</template>
+<script>
+  import Emitter from "../mixins/emitter";
+
+  export default {
+    name: "iCheckbox",
+    mixins: [ Emitter ],
+    props: {
+      disabled: {
+        type: Boolean,
+        default: false
+      },
+      value: {
+        type: [String, Number, Boolean],
+        default: false
+      },
+      trueValue: {
+        type: [String, Number, Boolean],
+        default: true
+      },
+      falseValue: {
+        type: [String, Number, Boolean],
+        default: false
+      }
+    },
+    data () {
+      return {
+        currentValue: this.value
+      }
+    },
+    methods: {
+      change(event) {
+        if (this.disabled) {
+          return false;
+        }
+        const checked = event.target.checked;
+        this.currentValue = checked
+        const value = checked ? this.trueValue : this.falseValue;
+        this.$emit("input", value);
+        this.$emit("on-change", value);
+        this.dispatch("iFromItem", "on-form-change", value);
+      },
+      updateModel() {
+        this.currentValue = this.value === this.trueValue;
+      }
+    },
+    watch: {
+      value(val) {
+        if (val === this.trueValue || val === this.falseValue) {
+          this.updateModel();
+        } else {
+          throw "Value should be trueValue or falseValue.";
+        }
+      }
+    },
+  }
+</script>
+<style>
+
+</style>
